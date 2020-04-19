538cea61c2694660a67d3e8e070505457d760f06
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2020-04-19 02:43:10 -0500

    Updates

--- build/animatable-component.js
+++ build/animatable-component.js
@@ -125,7 +125,7 @@ var StyleNode=function(){this.start=0,this.end=0,this.previous=null,this.parent=
 
   var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
   var start = function() {
-    var url = new URL('./p-1fd3fb60.system.js', resourcesUrl);
+    var url = new URL('./p-9bd64259.system.js', resourcesUrl);
     System.import(url.href);
   };
 
